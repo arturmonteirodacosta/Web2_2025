80e570517ba08cbfc4ff4edeab07ae942c537681
arturmonteirodacosta <arturmonteirodacosta@gmail.com>
2025-07-09 11:51:32 -0300

    Update script.js

--- selects/script.js
+++ selects/script.js
@@ -12,6 +12,8 @@ function contarOpcao(select) {
 }
 
 function lerSelects(){
+    quantidade_a=0;
+    quantidade_b=0;
     // Pega todos os <select> da página e guarda em uma variavel
     const selects = document.querySelectorAll('select');
     // Percorre cada select usando forEach (parecido com loop)
@@ -19,6 +21,17 @@ function lerSelects(){
     // Mostra o resultado no console depois de rodar "todo o loop"
     console.log('Quantidade de A:', quantidade_a);
     console.log('Quantidade de B:', quantidade_b);
+    mostrarResultado();
+    atualizarPorcentagem();
 }
-
-
+// Maneira simples de mostrar os resultados
+function mostrarResultado(){
+    document.getElementById("resultado").innerHTML = "Sua afinidade com exatas é de:"+quantidade_a;
+}
+// Mostrando com barra de porcentagem
+function atualizarPorcentagem(){
+    const skillBar = document.getElementById('exatas');
+    // Multiplicando por 10 pq temos 10 questões, mudem se tiverem números diferentes.
+    skillBar.style.width = quantidade_a*10+'%';
+    skillBar.textContent = quantidade_a*10+'%';
+}
